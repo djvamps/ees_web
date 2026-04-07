1ef0c6dfc9a819320c4fd1f922742f3b352ce98b
matt765 <mateusz.wyrebek@gmail.com>
2026-04-07 06:41:08 +0200

    Update all packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
