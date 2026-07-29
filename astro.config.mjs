0aecbad70c16be977679043ec9ce65ec09b84277
djvamps <dalopeza9@hotmail.com>
2026-07-29 18:35:04 -0500

    fix: agregar adaptador de Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://ecoescuelasoylibre.com',
+  output: 'static',
+  adapter: netlify(),
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
