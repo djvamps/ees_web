91542da4a07793134d942aa10a9a51b24599df95
djvamps <dalopeza9@hotmail.com>
2026-07-29 18:58:30 -0500

    fix: remover output hybrid para compatibilidad con adaptador netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://ecoescuelasoylibre.com',
-  output: 'hybrid',
   adapter: netlify(),
   integrations: [sitemap()],
   vite: {
